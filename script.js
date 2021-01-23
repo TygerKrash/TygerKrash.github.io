057e67c5fb3183f6ea5d6733286f9374168b1051
Dave McGowan <dave.mcgowan+github@gmail.com>
2021-01-23 13:05:21 +0000

    fix record id

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 const endpoint = "https://jsonbox.io/box_d19452d79b225319086b";
-const recordId = "600b6b3b92d72b0015b0b3e2";
+const recordId = "600c1e4a92d72b0015b0c10f";
 
 const ATTACK = "attack";
 const DEFEND = "defend";
