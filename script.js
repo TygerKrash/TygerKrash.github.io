261c2af3d0dd6a5f8df6d69eb7c4a785559594ad
Dave McGowan <dave.mcgowan+github@gmail.com>
2021-01-23 00:26:11 +0000

    debug

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 
 const endpoint = "https://jsonbox.io/box_d19452d79b225319086b"
 
-fetch(endpoint, (response) => {
+fetch(endpoint, function(response) {
 
     if(response.status = "200") {
         response.JSON().then( (data)=> console.log(data));
+    }else{
+        console.log(response);
     }
-}, (err) => console.log(err));
+}, function(err) { console.log(err);});
