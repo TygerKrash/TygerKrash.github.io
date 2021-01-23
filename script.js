0e2eb8b5b435ab190f7b00da2a137c7599a1b429
Dave McGowan <dave.mcgowan+github@gmail.com>
2021-01-23 00:47:08 +0000

    add some card logic

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 
-const endpoint = "https://jsonbox.io/box_d19452d79b225319086b"
+const endpoint = "https://jsonbox.io/box_d19452d79b225319086b";
+const recordId = "600b6b3b92d72b0015b0b3e2";
 
-fetch(endpoint).then( function(response) {
+fetch(`${endpoint}/${recordId}`).then( function(response) {
 
     if(response.status = "200") {
-        response.json().then( (data)=> console.log(data));
+        response.json().then( (data)=> drawCards(data));
     }else{
         console.log(response);
     }
 }, function(err) { console.log(err);});
+
+
+function drawCards(data){
+   data.action[0].action;
+   if(action == "attack") {
+     document.querySelector("card1").classList.add("attackCard");
+   }
+   if(action == "defend") {
+    document.querySelector("card1").classList.add("defendCard");
+   }
+   if(action == "maneuver") {
+    document.querySelector("card1").classList.add("maneuverCard");
+   }
+   if(action == "feint") {
+    document.querySelector("card1").classList.add("feintCard");
+   }
+}
