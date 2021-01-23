60d0fca77fc836b8afc4828110fc6a818b9f0ca3
Dave McGowan <dave.mcgowan+github@gmail.com>
2021-01-23 00:31:03 +0000

    fixed .json function.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const endpoint = "https://jsonbox.io/box_d19452d79b225319086b"
 fetch(endpoint).then( function(response) {
 
     if(response.status = "200") {
-        response.JSON().then( (data)=> console.log(data));
+        response.json().then( (data)=> console.log(data));
     }else{
         console.log(response);
     }
