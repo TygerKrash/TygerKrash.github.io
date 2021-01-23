1b8da5bea6b47470ab3205ade575f7b5612e093e
Dave McGowan <dave.mcgowan+github@gmail.com>
2021-01-23 13:08:22 +0000

    query selector fix

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 const endpoint = "https://jsonbox.io/box_d19452d79b225319086b";
 const recordId = "600c1e4a92d72b0015b0c10f";
 
+https://jsonbox.io/box_d19452d79b225319086b/600b6b3b92d72b0015b0b3e2
+
 const ATTACK = "attack";
 const DEFEND = "defend";
 const FEINT = "defend";
@@ -20,15 +22,15 @@ fetch(`${endpoint}/${recordId}`).then( function(response) {
 function drawCards(data){
    const action = data.action[0].action;
    if(action == ATTACK) {
-     document.querySelector("card1").classList.add("attackCard");
+     document.querySelector("#card1").classList.add("attackCard");
    }
    if(action == DEFEND) {
-    document.querySelector("card1").classList.add("defendCard");
+    document.querySelector("#card1").classList.add("defendCard");
    }
    if(action == FEINT) {
-    document.querySelector("card1").classList.add("maneuverCard");
+    document.querySelector("#card1").classList.add("maneuverCard");
    }
    if(action == MANEUVER) {
-    document.querySelector("card1").classList.add("feintCard");
+    document.querySelector("#card1").classList.add("feintCard");
    }
 }
