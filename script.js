b24bae9407b3c4b46221a837af5ee09628907c1a
Dave McGowan <dave.mcgowan+github@gmail.com>
2021-01-23 00:29:52 +0000

    fix fetch usage.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 const endpoint = "https://jsonbox.io/box_d19452d79b225319086b"
 
-fetch(endpoint, function(response) {
+fetch(endpoint).then( function(response) {
 
     if(response.status = "200") {
         response.JSON().then( (data)=> console.log(data));
